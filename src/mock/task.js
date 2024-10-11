c09562c42f8b3635f6d0606b030af9a7e09baf58
AlexDemeshkin <163833861+AlexDemeshkin@users.noreply.github.com>
2024-10-11 11:34:00 +0300

    Create task.js

---
+++ src/mock/task.js
@@ -0,0 +1,52 @@
+export const tasks =[
+    {
+        'id': `1`,
+        'title': `Выучить JS`,
+        'status': `blogack`
+    },
+    {
+        'id': `2`,
+        'title': `Выучить React`,
+        'status': `blogack`
+    },
+    {
+        'id': `3`,
+        'title': `Сделать домашку`,
+        'status': `blogack`
+    },
+    {
+        'id': `4`,
+        'title': `Выпить смузи`,
+        'status': `in_progress`
+    },
+    {
+        'id': `5`,
+        'title': `Позвонить маме`,
+        'status': `ready`
+    },
+    {
+        'id': `6`,
+        'title': `Погнладить кота`,
+        'status': `ready`
+    },
+    {
+        'id': `7`,
+        'title': `Сходить погулять`,
+        'status': `garbage`
+    },
+    {
+        'id': `8`,
+        'title': `Прочитать войну и мир`,
+        'status': `garbage`
+    },
+    {
+        'id': `9`,
+        'title': `Сдать лабораторную`,
+        'status': `garbage`
+    },
+    {
+        'id': `10`,
+        'title': `Сдать лабораторную`,
+        'status': `ready`
+    },
+];
