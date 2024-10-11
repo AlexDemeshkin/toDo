f1294d28d940cf401b212644fa040b98cff37ddc
AlexDemeshkin <163833861+AlexDemeshkin@users.noreply.github.com>
2024-10-11 11:26:12 +0300

    Create task-component.js

---
+++ src/view/task-component.js
@@ -0,0 +1,42 @@
+import {createElement} from '../framework/render.js';
+
+
+function createTaskComponentTemplate(task) {
+    const{status,title}=task;
+    return (
+`        
+        <ul class="list_${status}">
+        <li class="task--${status}">${title}</li>
+ </ul>
+        `
+
+      );
+}
+
+
+export default class TaskComponent {
+    constructor({ task } ) {
+        this.task = task;
+      }
+    
+      getTemplate() {
+        return createTaskComponentTemplate(this.task);
+      }
+
+ 
+
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+
+    return this.element;
+  }
+
+
+  removeElement() {
+    this.element = null;
+  }
+}
