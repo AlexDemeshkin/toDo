a1820653d6a510d66cdf2cff655a2243efbea387
AlexDemeshkin <163833861+AlexDemeshkin@users.noreply.github.com>
2024-10-16 13:44:44 +0300

    Revert "Update tasks-board-presenter.js"
    This reverts commit a14d978b948bbde833b13506e695db0591e7e676.

--- src/presenter/tasks-board-presenter.js
+++ src/presenter/tasks-board-presenter.js
@@ -1,55 +1,46 @@
 import TasksListComponent from '../view/tasks-list-component.js'
 import TaskComponent from '../view/task-component.js';
-import SubmitTaskComponent from '../view/submitTask-component.js'
 import TaskBoardComponent from '../view/tasks-board-component.js';
+import {render} from '../framework/render.js';
 import ClearButtonComponent from "../view/button-clear.js";
 import { Status} from '../const.js';
-import {render} from '../framework/render.js';
 
 export default class TasksBoardPresenter {
-  #boardContainer=null;
-  #tasksModel=null;
+ tasksBoardComponent = new TaskBoardComponent()
 
-  #boardTasks=[];
-
-  #tasksBoardComponent = new TaskBoardComponent()
 
  constructor({boardContainer, tasksModel}) {
-   this.#boardContainer = boardContainer;
-   this.#tasksModel=tasksModel;  
+   this.boardContainer = boardContainer;
+   this.tasksModel=tasksModel;  
  }
 
 init() {
-    this.#boardTasks = [...this.#tasksModel.getTasks()];
-  render(this.#tasksBoardComponent, this.#boardContainer);  
+  this.boardTasks = [...this.tasksModel.getTasks()];
+ render(this.tasksBoardComponent, this.boardContainer);  
+  
+ for (let el in Status) {
+  this.status_title = Status[el];
+  const tasksListComponent = new TasksListComponent({status: Status[el]});
 
-  for (let el in Status) {
-          this.status_title = Status[el];
-          const tasksListComponent = new TasksListComponent({status: Status[el]});
+     render(tasksListComponent, this.tasksBoardComponent.getElement());
 
-            render(tasksListComponent, this.#tasksBoardComponent.element);
-          if (tasksListComponent.length===0){
-            render(new SubmitTaskComponent(), tasksListComponent.element());
-          }
-          else{
-            for (let j = 0; j <  this.#boardTasks.length; j++) {
-              this.#renderTask(
-                this.#boardTasks[j],
-                tasksListComponent.element,
-                this.status_title)
-            }
-          }
-        }
-      this.makeClearButton();
-      }
-      #renderTask(task, container, status_title) {
-        const taskComponent = new TaskComponent({ task: task });
-        if (task.status == status_title) {
-          render(taskComponent, container);
-        }
-      }  
-    makeClearButton() {
-      const basketContainer = document.querySelector(`.list_${Status.BASKET}`);
-      render(new ClearButtonComponent(), basketContainer);
-  }
+     for (let j = 0; j <  this.boardTasks.length; j++) {
+         const taskComponent = new TaskComponent({task: this.boardTasks[j]});
+        
+         if (this.boardTasks[j].status == this.status_title)
+         {
+         render(taskComponent, tasksListComponent.getElement());
+         }
+     }
+
+ }
+ this.makeClearButton();
+
+}
+makeClearButton() {
+  const basketContainer = document.querySelector(`.${Status.BASKET}`);
+  
+  render(new ClearButtonComponent(), basketContainer);
+}
+
 }
