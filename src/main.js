164429ba3334766b910210921af3cc01ede1691b
AlexDemeshkin <163833861+AlexDemeshkin@users.noreply.github.com>
2024-11-03 19:03:27 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import HeaderComponent from './view/header-component.js';
 import FormComponent from './view/form-component.js';
 import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
 import TasksModel from "./model/task-model.js";
-
 import {render, RenderPosition} from './framework/render.js';
 
 
