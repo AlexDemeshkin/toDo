6bdcc64b24b6ef3ae2166a4f8f372cfc4c4bd2b8
AlexDemeshkin <163833861+AlexDemeshkin@users.noreply.github.com>
2024-11-08 21:22:10 +0300

    Update button-clear.js

--- src/view/button-clear.js
+++ src/view/button-clear.js
@@ -1,22 +1,26 @@
-import { createElement } from "../framework/render.js";
+import { AbstractComponent } from "../framework/view/abstract-component.js";
 
 function createClearButtonComponentTemplate() {
   return `
-                <button type="button">x Очистить</button>
-        `;
+    <button type="button">x Очистить</button>
+  `;
 }
 
-export default class ClearButtonComponent{
-  getTemplate() {
+export default class ClearButtonComponent extends AbstractComponent {
+  #handleClick = null;
+
+  constructor({ onClick }) {
+    super();
+    this.#handleClick = onClick;
+    this.element.addEventListener("click", this.#clickHandler); 
+  }
+
+  get template() {
     return createClearButtonComponentTemplate();
   }
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-    return this.element;
-  }
-  removeElement() {
-    this.element = null;
-  }
+
+  #clickHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleClick();
+  };
 }
