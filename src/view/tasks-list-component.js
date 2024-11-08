034ec596a6b903c51f311ad6218fc9f94a56d4df
AlexDemeshkin <163833861+AlexDemeshkin@users.noreply.github.com>
2024-11-08 21:38:32 +0300

    Update tasks-list-component.js

--- src/view/tasks-list-component.js
+++ src/view/tasks-list-component.js
@@ -1,46 +1,26 @@
-import {createElement} from '../framework/render.js';
+import { AbstractComponent } from "../framework/view/abstract-component.js";
 import { StatusLabel } from "../const.js";
 
 function createTasksListComponentTemplate(status) {
     return (
-      
-      `
-       <div class="${status}">
-                <label>${StatusLabel[status]}</label>
-               
-            
-        </div>            
-      
+      `       
+      <ul class="list_${status}">
+                      <label>${StatusLabel[status]}</label> 
+        </ul>
      
     `
-
       );
 }
 
-
-export default class tasksListComponent {
+export default class tasksListComponent extends AbstractComponent {
   status;
 
   constructor({ status }) {
+    super();
     this.status = status;
   }
 
-  getTemplate() {
+  get template() {
     return createTasksListComponentTemplate(this.status);
-  }
-
-
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-
-
-    return this.element;
-  }
-
-
-  removeElement() {
-    this.element = null;
   }
 }
